2335d3650ee4499cd4f8ea9fe5eeae048ebbc59d
Matias <mvaleriani@fi.uba.ar>
2025-03-12 23:32:08 -0300

    Modifioc loggin config

--- app/logging_config.py
+++ app/logging_config.py
@@ -1,20 +1,17 @@
-from loguru import logger # type: ignore
+from loguru import logger  # type: ignore
 import sys
 import os
 
-# Crear el directorio de logs si no existe
 log_directory = "logs"
 if not os.path.exists(log_directory):
     os.makedirs(log_directory)
 
-# Configuración básica de loguru
-logger.remove()  # Elimina cualquier configuración previa
+logger.remove()
 logger.add(sys.stdout, format="{time} {level} {message}", level="INFO")
 logger.add(
     f"{log_directory}/app.log", rotation="1 MB", retention="10 days", level="DEBUG"
 )
 
 
-# Función para obtener el logger configurado
 def get_logger():
     return logger
